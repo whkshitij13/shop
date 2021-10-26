ad837df277da3479642f8c0d2290874b824f65d9
whkshitij13 <69427831+whkshitij13@users.noreply.github.com>
2021-10-26 21:29:56 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
+import {commerce} from  './lib/commerce';
 import { Navbar ,Products} from "./components";
 const App = () => {
+	const[product,setProducts]=useState([]);
+
+	const fetchProducts=async ()=>{
+		const 
+	}
 	return (
 		<div>
 			<Navbar />
