3a7f6837e12ba130a77b6701a21abd485075a097
whkshitij13 <69427831+whkshitij13@users.noreply.github.com>
2021-10-30 10:48:39 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from "react";
+import {useState,useEffect} from "react";
 import {commerce} from  './lib/commerce';
-import { Navbar ,Products} from "./components";
+import { Navbar,Products } from "./components";
 const App = () => {
-	const[product,setProducts]=useState([]);
+	const[products,setProducts]=useState([]);
 
-	const fetchProducts=async ()=>{
-		const 
+	const fetchProducts= async ()=>{
+	const{data}=await commerce.products.list();
+
+	setProducts(data);
 	}
+	useEffect(()=>{
+		fetchProducts();
+	},[]);
+
+
+	
 	return (
 		<div>
 			<Navbar />
-			<Products/>
+			<Products products={products}/>
 		</div>
 	);
 };
+
 
 export default App;
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -9,6 +9,7 @@ const useStyles = makeStyles((theme) => ({
 		[theme.breakpoints.up("sm")]: {
 			width: `calc(100% - ${drawerWidth}px)`,
 			marginLeft: drawerWidth,
+			
 		},
 	},
 	title: {
